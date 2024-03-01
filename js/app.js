262c74bcd0c039c1ad7eaf1215e5828c77304654
halee-t <haleetisler@gmail.com>
2024-02-29 22:51:07 -0500

    Count down instead of clock, adding previously as well. Need to test still

--- js/app.js
+++ js/app.js
@@ -1,8 +1,36 @@
 var app = angular.module("grizzHacksSchedule", []);
 
-app.controller("ScheduleCtrl", function ($scope, $http, $interval) {
+app.filter("pad", function () {
+  return function (input) {
+    return input < 10 ? "0" + input : "" + input;
+  };
+});
+
+app.controller("ScheduleCtrl", function ($scope, $http, $interval, $filter) {
   $scope.events = [];
-  //Fetch schedule data from grizzhacks6_schedule.json
+  $scope.hour = 0;
+  $scope.min = 0;
+  $scope.sec = 0;
+
+  //COUNT DOWN CODE
+  var targetDate = new Date("March 10, 2024 12:30:00 GMT-0400"); // Adjust for your timezone
+
+  function calculateCountdown() {
+    var now = new Date();
+    var timeDiff = targetDate - now;
+
+    if (timeDiff <= 0) {
+      return { hours: 0, minutes: 0, seconds: 0 };
+    }
+
+    var hours = Math.floor(timeDiff / (1000 * 60 * 60));
+    var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+    $scope.hour = hours;
+    $scope.min = minutes;
+    $scope.sec = seconds;
+  }
 
   function loadSchedule() {
     $http
@@ -17,88 +45,78 @@ app.controller("ScheduleCtrl", function ($scope, $http, $interval) {
       });
   }
 
-  function showTime() {
-    // Getting current time and date
-    var time = new Date();
-    var hour = time.getHours();
-    var min = time.getMinutes();
-    var sec = time.getSeconds();
-    var am_pm = "AM";
-
-    // Setting time for 12 Hrs format
-    if (hour >= 12) {
-      if (hour > 12) hour -= 12;
-      am_pm = "PM";
-    } else if (hour == 0) {
-      hour = 12;
-      am_pm = "AM";
-    }
-
-    hour = hour < 10 ? "0" + hour : hour;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
-
-    // Update the scope variables to be used in the view
-    $scope.hour = hour;
-    $scope.min = min;
-    $scope.sec = sec;
-  }
-
   function updateCurrentEvents() {
     var currentTime = new Date();
 
+    $scope.previousEvent = angular.copy($scope.currentEvent);
     $scope.currentEvent = findCurrentEvent(currentTime);
+
+    // Check if currentEvent has changed, and update previouslyEvent accordingly
+    if (!angular.equals($scope.currentEvent, $scope.previouslyEvent)) {
+      $scope.previouslyEvent = angular.copy($scope.previouslyEvent);
+    }
+
     $scope.nextEvent = findNextEvent(currentTime);
+  }
+
+  function parseTimeString(dateString, timeString) {
+    // Combine the date and time strings
+    var dateTimeString = dateString + " " + timeString;
+    // Create a new Date object
+    var date = new Date(dateTimeString);
+    return date;
   }
 
   function findCurrentEvent(currentTime) {
     for (var day in $scope.events) {
       for (var i = 0; i < $scope.events[day].length; i++) {
         var event = $scope.events[day][i];
-        var eventTime = parseEventTime(event.time, day);
+        var startTime = parseTimeString(day, event.startTime);
+        var endTime = parseTimeString(day, event.endTime);
 
-        if (currentTime >= eventTime.start && currentTime < eventTime.end) {
+        if (
+          startTime &&
+          endTime &&
+          currentTime >= startTime &&
+          currentTime < endTime
+        ) {
+          event.formattedStartTime = $filter("date")(startTime, "shortTime");
+          event.formattedEndTime = $filter("date")(endTime, "shortTime");
           return event;
         }
       }
     }
 
-    return "I'm here too";
+    //if you didn't find an event, return null
+    return null;
   }
 
   function findNextEvent(currentTime) {
     for (var day in $scope.events) {
       for (var i = 0; i < $scope.events[day].length; i++) {
         var event = $scope.events[day][i];
-        var eventTime = parseEventTime(event.time, day);
+        var startTime = parseTimeString(day, event.startTime);
+        var endTime = parseTimeString(day, event.endTime);
 
-        if (currentTime < eventTime.start) {
+        if (startTime && currentTime < startTime) {
+          event.formattedStartTime = $filter("date")(startTime, "shortTime");
+          event.formattedEndTime = $filter("date")(endTime, "shortTime");
           return event;
         }
       }
     }
 
-    return "I'm here";
-  }
-
-  function parseEventTime(eventTime, day) {
-    var times = eventTime.split(" - ");
-    var startDate = new Date(day + " " + times[0]);
-    var endDate = new Date(day + " " + times[1]);
-
-    return { start: startDate, end: endDate };
+    return null;
   }
 
   $scope.currentEvent = {};
   $scope.nextEvent = {};
 
   // Initial call to loadSchedule
+  calculateCountdown();
   loadSchedule();
-  showTime();
 
-  // Refresh every second
-  $interval(showTime, 1000);
+  $interval(calculateCountdown, 1000);
 
   $interval(loadSchedule, 60 * 1000);
-  $interval(updateCurrentEvents, 1000);
 });
